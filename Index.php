f1198e90d83144fda764d31ff88764adb3006b0f
leonardoabreu-si <94482550+leonardo-trompista@users.noreply.github.com>
2026-01-14 07:39:44 -0300

    Create Index.php
    Primeiro teste de php

---
+++ Index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ateliê</title>
+
+    <!-- Estilos -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <header>
+        <h1>Ateliê</h1>
+    </header>
+
+    <main>
+        <p><?php echo 'Testando o poder do php'; ?></p>
+    </main>
+
+    <footer>
+        <p>&copy; 2026 - Ateliê</p>
+    </footer>
+
+</body>
+</html>
