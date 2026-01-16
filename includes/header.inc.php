e26b228ea66df1608ad4300f0bf50a7c79cd4b0f
leonardoabreu-si <94482550+leonardo-trompista@users.noreply.github.com>
2026-01-16 00:30:15 -0300

    limpeza do header

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,22 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/simple.css" />
-    <link rel="stylesheet" href="./styles/custom.css" />
-    <title>Culinary Cove &bull; <?php echo $pageTitle; ?></title>
-</head>
-<body>
   <header class="header-with-background" style="background-image: url('<?php echo $headerImg; ?>'); ">
     <h1>Culinary Cove</h1>
     <p>Your sanctuary for exceptional flavors</p>
     <nav>
       
-      
-      <a href="ingredients.php">Ingredients</a>
+      <a href="?page=ingredients">Ingredients</a>
+
      
     </nav>
   </header>
-
-  <main>
